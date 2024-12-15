d9949c07931c1b23f643e0e0a27b665124fde811
Александр <116272931+Scorpic99@users.noreply.github.com>
2024-12-15 08:54:50 +0300

    Add files via upload

---
+++ module_13_1.py
@@ -0,0 +1,24 @@
+import asyncio
+import time
+
+
+async def start_strongman(name, power):
+    spheres = 5
+    print(f'Силач {name} начал соревнования.')
+    for sphere in range(spheres):
+        await asyncio.sleep(1000 / power)
+        print(f'Силач {name} поднял {sphere + 1}')
+
+async def start_tournament():
+    task1 = asyncio.create_task(start_strongman('Lev', 200))
+    task2 = asyncio.create_task(start_strongman('Mark', 1000))
+    task3 = asyncio.create_task(start_strongman('Alexio', 500))
+    await task1
+    await task2
+    await task3
+
+start = time.time()
+asyncio.run(start_tournament())
+finish = time.time()
+
+print(f"Program time: {finish - start}")
